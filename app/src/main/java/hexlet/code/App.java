21b00cb7023af5a347fff54f05660bc164125b00
Valentin Shabarchin <dronanval@gmail.com>
2024-02-03 14:15:31 +0400

    add two parameters (filepath1, filepath2) and one option (format) with default value "stylish"

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,13 +2,25 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
+import java.nio.file.Path;
 import java.util.concurrent.Callable;
 
 @Command(name = "gendiff",
         mixinStandardHelpOptions = true,
         description = "Compares two configuration files and shows a difference.")
 public class App implements Callable<Integer> {
+
+    @Parameters(index = "0", paramLabel = "filepath1", description = "Path to first file")
+    private Path filepath1;
+
+    @Parameters(index = "1", paramLabel = "filepath2", description = "Path to second file")
+    private Path filepath2;
+
+    @Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format [default: stylish]")
+    private String format = "stylish";
 
     @Override
     public Integer call() throws Exception {
